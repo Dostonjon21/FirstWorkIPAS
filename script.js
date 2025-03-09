81a5319ad41da2d1bf9cf5c85eab39de716388e6
Doston12 <rajabovdost@gmail.com>
2025-03-09 14:20:02 +0500

    add

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict'
 const btn = document.querySelector('.btn-country')
 const countriesContainer = document.querySelector('.countries')
-
 const request = new XMLHttpRequest()
 request.open('GET','https://restcountries.com/v3.1/name/uzbekistan')
 request.send()
+request.addEventListener("load" () <{
+
+})
