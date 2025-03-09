2d7e1b02574eec7f9aba6f278c45ac6c028d116d
Doston12 <rajabovdost@gmail.com>
2025-03-09 14:24:34 +0500

    add

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const countriesContainer = document.querySelector('.countries')
 const request = new XMLHttpRequest()
 request.open('GET','https://restcountries.com/v3.1/name/uzbekistan')
 request.send()
-request.addEventListener("load" () <{
+request.addEventListener("load", () => {
+const info = JSON.parse(request.responseText)
+console.log(info);
 
 })
