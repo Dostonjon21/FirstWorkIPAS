48f9492ed55e93ef847c86cc1ce3f3e0c62d614d
Doston12 <rajabovdost@gmail.com>
2025-03-09 14:29:15 +0500

    add

--- script.js
+++ script.js
@@ -5,7 +5,17 @@ const request = new XMLHttpRequest()
 request.open('GET','https://restcountries.com/v3.1/name/uzbekistan')
 request.send()
 request.addEventListener("load", () => {
-const info = JSON.parse(request.responseText)
-console.log(info);
-
+const [info] = JSON.parse(request.responseText)
+const html='
+  <article>
+                <img src="" class="country-img">
+                <div class="country-data">
+                    <h3 class="country-name">Country</h3>
+                    <h4 class="country-region">Region</h4>
+                     <p class="country-row"><span>👨‍👩‍👦</span>POP people</p>
+                     <p class="country-row"><span>🗣</span>lANG</p>
+                     <p class="country-row"><span>💸</span>CUR</p>
+                </div>
+              </article>
+'
 })
