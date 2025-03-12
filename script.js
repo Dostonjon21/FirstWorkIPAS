3bb9f67920f5cf7254875dd18ef9aea99467b35e
Doston12 <rajabovdost@gmail.com>
2025-03-12 17:28:04 +0500

    ADD

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const renderCountry = function (info) {
-    const borders = info.borders ? info.borders.join(', ') : 'No borders';
+const renderCountry = (info) => {
+    const borders = info.borders?.join(', ') || 'No borders';
     const html = `
         <article class="country">
             <img src="${info.flags.png}" class="country-img">
@@ -12,7 +12,7 @@ const renderCountry = function (info) {
                 <h3 class="country-name">${info.name.common}</h3>
                 <h4 class="country-region">${info.region}</h4>
                 <p class="country-row"><span>Population: </span>${(info.population / 1_000_000).toFixed(1)} million</p>
-                <p class="country-row"><span>Capital: </span>${info.capital ? info.capital[0] : 'No capital'}</p>
+                <p class="country-row"><span>Capital: </span>${info.capital?.[0] || 'No capital'}</p>
                 <p class="country-row"><span>Borders: </span>${borders}</p>
             </div>
         </article>
@@ -21,32 +21,32 @@ const renderCountry = function (info) {
     countriesContainer.style.opacity = 1;
 };
 
-const getCountryData = async function (country) {
-    try {
-        const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
-        const [info] = await res.json();
+const getCountryData = (country) => {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+    request.addEventListener('load', function () {
+        const info = JSON.parse(this.responseText)[0];
         renderCountry(info);
-
-        if (!info.borders) return;
-
-        const firstNeighbour = info.borders[0];
-        if (!firstNeighbour) return;
-
-        const res2 = await fetch(`https://restcountries.com/v3.1/alpha/${firstNeighbour}`);
-        const [neighbourInfo] = await res2.json();
-        renderCountry(neighbourInfo);
-
-        if (!neighbourInfo.borders) return;
-
-        const secondNeighbour = neighbourInfo.borders[0];
-        if (!secondNeighbour) return;
-
-        const res3 = await fetch(`https://restcountries.com/v3.1/alpha/${secondNeighbour}`);
-        const [secondNeighbourInfo] = await res3.json();
-        renderCountry(secondNeighbourInfo);
-    } catch (err) {
-        console.error(err);
-    }
+        
+        const firstNeighbour = info.borders?.[0] || '';
+        const request2 = new XMLHttpRequest();
+        request2.open('GET', `https://restcountries.com/v3.1/alpha/${firstNeighbour}`);
+        request2.send();
+        request2.addEventListener('load', function () {
+            const neighbourInfo = JSON.parse(this.responseText)[0];
+            renderCountry(neighbourInfo);
+            
+            const secondNeighbour = neighbourInfo.borders?.[0] || '';
+            const request3 = new XMLHttpRequest();
+            request3.open('GET', `https://restcountries.com/v3.1/alpha/${secondNeighbour}`);
+            request3.send();
+            request3.addEventListener('load', function () {
+                const secondNeighbourInfo = JSON.parse(this.responseText)[0];
+                renderCountry(secondNeighbourInfo);
+            });
+        });
+    });
 };
 
 getCountryData('Uzbekistan');
